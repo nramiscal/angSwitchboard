a06fb5ff52fffc1ee9519053c55c23e9205ecff7
nramiscal <nramiscal@gmail.com>
2017-12-05 22:22:08 -0500

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  objArray = [];
+  switchObj = {};
+
+  fillObjArray() {
+    for (let i=0; i < 10; i++){
+      this.switchObj = {
+        on: true,
+        color: 'green',
+        text: 'ON'
+      }
+      this.objArray.push(this.switchObj);
+    }
+  }
+
+  flip_switch(idx:number){
+
+    if (this.objArray[idx].on){
+      this.objArray[idx].on = false;
+      this.objArray[idx].color = 'red';
+      this.objArray[idx].text = 'OFF';
+    }
+    else {
+      this.objArray[idx].on = true;
+      this.objArray[idx].color = 'green';
+      this.objArray[idx].text = 'ON';
+
+    }
+    console.log(`flip_switch invoked for index ${idx}`);
+  }
+
+  ngOnInit() {
+    this.fillObjArray();
+
+  }
+}
